fff4fce43f23e11a7a220eea3ad7945db2d70b7d
Lusan Das <lusandas@Lusans-MacBook-Pro.local>
2023-09-18 10:35:18 +0530

    feat: init commmit

--- src/api/queueAPIInterceptor.js
+++ src/api/queueAPIInterceptor.js
@@ -2,57 +2,69 @@ const apiQueue = [];
 let inProgress = false;
 
 const queueBaseURLs = [
-  "https://api.example.com"
-  // ... add more base URLs as needed
+    'https://api.example.com',
+    'https://another-api.example.com',
+    // ... add more base URLs as needed
 ];
 
 const processQueue = async () => {
-  if (inProgress) return;
-  if (apiQueue.length === 0) return;
+    if (inProgress || apiQueue.length === 0) return;
 
-  inProgress = true;
+    inProgress = true;
 
-  try {
-    await apiQueue.shift()();
-  } catch (error) {
-    console.error("Error processing API queue:", error);
-  }
+    const { apiCall, resolver } = apiQueue.shift();
 
-  inProgress = false;
-  processQueue();
+    try {
+        const response = await apiCall();
+        resolver(response);  // Resolve the promise for this API call
+    } catch (error) {
+        console.error('Error processing API queue:', error);
+        resolver(Promise.reject(error)); // Reject the promise for this API call
+    }
+
+    inProgress = false;
+    processQueue();
 };
 
-const queueAPIInterceptor = (axiosInstance) => {
-  axiosInstance.interceptors.request.use(
-    (config) => {
-      if (queueBaseURLs.includes(config.baseURL)) {
-        const apiCall = () => axiosInstance(config);
-        apiQueue.push(apiCall);
+const setupInterceptors = (axiosInstance) => {
+    axiosInstance.interceptors.request.use(
+        (config) => {
+            if (queueBaseURLs.includes(config.baseURL)) {
+                // A mechanism to let each request know when it's its turn
+                let resolver;
+                const promise = new Promise((resolve) => {
+                    resolver = resolve;
+                });
+                console.log(config.baseURL)
+                apiQueue.push({
+                    apiCall: () => axiosInstance(config),
+                    resolver
+                });
 
-        if (!inProgress) {
-          processQueue();
+                if (!inProgress) {
+                    processQueue();
+                }
+
+                return promise; 
+            }
+
+            return config;
+        },
+        (error) => {
+            return Promise.reject(error);
         }
+    );
 
-        return new Promise(() => {});
-      }
+    axiosInstance.interceptors.response.use(
+        (response) => {
+            processQueue();
+            return response;
+        },
+        (error) => {
+            processQueue();
+            return Promise.reject(error);
+        }
+    );
+}
 
-      return config;
-    },
-    (error) => {
-      return Promise.reject(error);
-    }
-  );
-
-  axiosInstance.interceptors.response.use(
-    (response) => {
-      processQueue();
-      return response;
-    },
-    (error) => {
-      processQueue();
-      return Promise.reject(error);
-    }
-  );
-};
-
-export default queueAPIInterceptor;
+export default setupInterceptors;
